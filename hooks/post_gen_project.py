c54286a4e5afab3ef0c3ab95d0b26b8da1b8ee89
kpennels <keaton@dotmodus.com>
2021-03-08 09:38:52 +0200

    added logging

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -7,8 +7,9 @@ def get_random_secret_key():
     length = 50
     return ''.join(random.choice(allowed_chars) for _ in range(length))
 
-
-with open('{{cookiecutter.github_repository_name}}/.env', 'w') as env_file:
+import os
+print(os.listdir('.'))
+with open('../{{cookiecutter.github_repository_name}}/.env', 'w') as env_file:
     env_file.write(
         f"SECRET_KEY={get_random_secret_key()}\n"
         f"DATABASE_ENGINE=django.db.backends.postgresql\n"
