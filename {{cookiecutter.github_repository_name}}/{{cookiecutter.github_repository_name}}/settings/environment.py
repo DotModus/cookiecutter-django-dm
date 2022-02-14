427d60cb79979da8394490383d859eab68803994
AndrewModus <andrew@dotmodus.com>
2022-02-14 13:37:27 +0200

    Use pathlib method for checking file exists.

--- {{cookiecutter.github_repository_name}}/{{cookiecutter.github_repository_name}}/settings/environment.py
+++ {{cookiecutter.github_repository_name}}/{{cookiecutter.github_repository_name}}/settings/environment.py
@@ -4,8 +4,6 @@ Execution environment related settings.
 The environment variable reader (env)  is instantiated in this module and then
 imported into the other settings definition files.
 """
-import os
-
 import environ
 
 
@@ -15,7 +13,7 @@ CURRENT_PATH = environ.Path(__file__) - 1
 SITE_ROOT = CURRENT_PATH - 2
 env_file = SITE_ROOT(".env")
 
-if os.path.exists(env_file):
+if env_file.exists(env_file):
     environ.Env.read_env(env_file=env_file)
 else:
     print("No env file found")
