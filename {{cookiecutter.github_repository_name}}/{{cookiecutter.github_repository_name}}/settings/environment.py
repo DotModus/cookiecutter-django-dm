84a1cbfcb48d932eaad8ab546d9c1f2b6969d21c
AndrewModus <andrew@dotmodus.com>
2022-02-14 15:56:49 +0200

    General improvement update (#2)
    * Rename the api_config to the project name. As how django does it.
    
    * Increase versions of django and python to 4.0.2 and 3.10 respectively.
    
    * Add logging.
    
    * Use pathlib method for checking file exists.
    
    * Remove redundant, unused postgresql checker script.
    
    * Install app to /opt/{{github_repository_name}}.
    
    * Update all requirements.
    
    * New line at end of file.
    
    * Add the django app to the docker-compose setup.
    
    * Use the standard Pathlib library for file types.

--- {{cookiecutter.github_repository_name}}/{{cookiecutter.github_repository_name}}/settings/environment.py
+++ {{cookiecutter.github_repository_name}}/{{cookiecutter.github_repository_name}}/settings/environment.py
@@ -4,18 +4,18 @@ Execution environment related settings.
 The environment variable reader (env)  is instantiated in this module and then
 imported into the other settings definition files.
 """
-import os
+from pathlib import Path
 
 import environ
 
 
 env = environ.Env(DEBUG=(bool, False))
 
-CURRENT_PATH = environ.Path(__file__) - 1
-SITE_ROOT = CURRENT_PATH - 2
-env_file = SITE_ROOT(".env")
+CURRENT_PATH = Path(__file__).parent
+SITE_ROOT = CURRENT_PATH.parent.parent
+env_file = SITE_ROOT / ".env"
 
-if os.path.exists(env_file):
+if env_file.exists():
     environ.Env.read_env(env_file=env_file)
 else:
     print("No env file found")
