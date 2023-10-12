a554fcb21b4e3be437e87e854fbff87b2ebd913a
tasimba <taseemba>
2023-10-12 08:07:47 +0200

    remove funny characters

--- {{cookiecutter.github_repository_name}}/apps/core/viewsets.py
+++ {{cookiecutter.github_repository_name}}/apps/core/viewsets.py
@@ -1,10 +1,7 @@
 # Create your viewsets here.
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-<<<<<<< HEAD
-=======
 
->>>>>>> 2f8b90d (update files)
 from . import models
 from . import serializers
 
